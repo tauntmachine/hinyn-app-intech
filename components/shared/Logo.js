3d5c55cfc0f91591a9bf7480050a918c813180e9
bluelupinemary <marygraceaguirre@gmail.com>
2023-03-25 01:43:09 +0400

    feat: update logo

--- components/shared/Logo.js
+++ components/shared/Logo.js
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
-import LogoImage from "/public/assets/img/logo-hinyn-v2.svg";
+import LogoImage from "/public/assets/img/logo-normal.svg";
 import WhiteLogoImage from "/public/assets/img/logo-hinyn-white.svg";
 import { useRouter } from "next/router";
 
