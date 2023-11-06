98386d746daac5a77a2997ad7158e80eeccc6798
tauntmachine <khazanmir666@gmail.com>
2023-11-07 02:53:07 +0500

    fix production error

--- pages/loading.js
+++ pages/loading.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
-import { useEffect, useRouter } from 'react';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Loading() {
   const router = useRouter();
 
